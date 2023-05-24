c864cee5ad276443a9fa74a6525335bb8247b31c
elie.aigon <elie.aigon@laplateforme.io>
2023-05-24 16:59:37 +0200

    Project done

--- src/GlobalConverter.java
+++ src/GlobalConverter.java
@@ -1,51 +1,99 @@
-import java.io.OutputStream;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GlobalConverter {
-    public static void main(String[] args) {
-        String str = args[1];
-        String ouput;
-        switch (args[0]) {
-            case "-h":
-            case "hexadecimal":
-                ToHexa TH = new ToHexa();
-                ouput = TH.StringToHex(CheckInput(str));
-                System.out.println(ouput);
-                break;
-            case "-d":
-            case "decimal":
-                ToDecimal TD = new ToDecimal();
-                ouput = TD.StringToDecimal(CheckInput(str));
-                System.out.println(ouput);
-                break;
-            case "-o":
-            case "octal":
-                ToOctal TO = new ToOctal();
-                ouput = TO.StringToOctal(CheckInput(str));
-                System.out.println(ouput);
-                break;
-            case "-b":
-            case "binary":
-                ToBinary TB = new ToBinary();
-                ouput = TB.StringToBinary(CheckInput(str));
-                System.out.println(ouput);
-                break;
-            case "-t":
-            case "text":
+    private static Map<Character, String> DicFirstStep = new HashMap<>();
+    private static Map<Character, String> DicFinalStep = new HashMap<>();
 
-            default:
-                System.out.println("Wrong options spelling.\n" +
-                        "First argument spelling :\n" +
-                        "'-h' = convert to hexadecimal.\n" +
-                        "'-o' = convert to octal.\n" +
-                        "'-b' = convert to binary.\n" +
-                        "'-d' = convert to decimal.\n" +
-                        "Make sure to quote your string correctly.");
-        }
+    public static void main(String[] args) {
+        String input = CheckInput(args[0]);
+        String output1;
+        String output2;
+        output1 = ConvertFirstStep(input, args[1]);
+        output2 = ConvertFinalStep(output1, args[2]);
+        System.out.println("Output : " + output2);
     }
     public static String CheckInput(String input) {
         return input.replaceAll("[^a-zA-Z0-9 ]","");
     }
-
-
+    public static String ConvertFirstStep(String input, String args1) {
+        StringBuilder output = new StringBuilder();
+        switch(args1) {
+            case "-h":
+            case "hexadecimal":
+                DicFirstStep = Dic.GetDicHexa();
+                for (String x : input.split(" ")) {
+                    for (Map.Entry<Character, String> entry : DicFirstStep.entrySet()) {
+                        if (entry.getValue().equals(x)) {
+                            output.append(entry.getKey());
+                        }
+                    }
+                }return output.toString();
+            case "-o":
+            case "octal":
+                DicFirstStep = Dic.GetDicOctal();
+                for (String x : input.split(" ")) {
+                    for (Map.Entry<Character, String> entry : DicFirstStep.entrySet()) {
+                        if (entry.getValue().equals(x)) {
+                            output.append(entry.getKey());
+                        }
+                    }
+                }return output.toString();
+            case "-b":
+            case "binary":
+                DicFirstStep = Dic.GetDicBinary();
+                for (String x : input.split(" ")) {
+                    for (Map.Entry<Character, String> entry : DicFirstStep.entrySet()) {
+                        if (entry.getValue().equals(x)) {
+                            output.append(entry.getKey());
+                        }
+                    }
+                }return output.toString();
+            case "-d":
+            case "decimal":
+                DicFirstStep = Dic.GetDicDecimal();
+                for (String x : input.split(" ")) {
+                    for (Map.Entry<Character, String> entry : DicFirstStep.entrySet()) {
+                        if (entry.getValue().equals(x)) {
+                            output.append(entry.getKey());
+                        }
+                    }
+                }return output.toString();
+            default:
+                return input;
+        }
+    }
+    public static String ConvertFinalStep(String input, String args2) {
+        StringBuilder output = new StringBuilder();
+        switch(args2) {
+            case "-h":
+            case "hexadecimal":
+                DicFinalStep = Dic.GetDicHexa();
+                for (int i = 0; i< input.length(); i++) {
+                    output.append(DicFinalStep.get(input.charAt(i)) + " ");
+                }return output.toString();
+            case "-o":
+            case "octal":
+                DicFinalStep = Dic.GetDicOctal();
+                for (int i = 0; i< input.length(); i++) {
+                    output.append(DicFinalStep.get(input.charAt(i)) + " ");
+                }return output.toString();
+            case "-b":
+            case "binary":
+                DicFinalStep = Dic.GetDicBinary();
+                for (int i = 0; i< input.length(); i++) {
+                    output.append(DicFinalStep.get(input.charAt(i)) + " ");
+                }return output.toString();
+            case "-d":
+            case "decimal":
+                DicFinalStep = Dic.GetDicDecimal();
+                for (int i = 0; i< input.length(); i++) {
+                    output.append(DicFinalStep.get(input.charAt(i)) + " ");
+                }return output.toString();
+            default:
+                output.append(input);
+                return output.toString();
+        }
+    }
 }
+
